306cce4c19a0c4925d4bb10e009f2004b908190c
mosmn <ithinkisee8@gmail.com>
2022-11-17 07:04:34 +0800

    wrote the algorithm for the first 2 steps to test it

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+PROJECT: create a sketchpad with controls like grid size, color, eraser and a reset
+
+ IOFC:
+      Input :
+      1) click(hold down)
+      2) grid size
+
+
+      Output:
+      1) box color
+
+
+      Formula: n/a
+
+
+      Condition:
+      1) keep adding color to boxes while user is holding down the mouse click
+
+
+ Algorithm:
+ 
+ BEGIN 
+    1) create a 16 x 16 grid by creating a element under class sketchpad
+    2) prompt the user for a number less than 100 and change the grid size according to user input
+ END 
+
+*/ 
