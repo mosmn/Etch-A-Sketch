86a6e0e6900d5f7a1362cd6a44205096a4d3c188
mosmn <ithinkisee8@gmail.com>
2022-11-17 08:31:40 +0800

    done with the basic sketchpad

--- main.js
+++ main.js
@@ -22,26 +22,48 @@ PROJECT: create a sketchpad with controls like grid size, color, eraser and a re
  
  BEGIN 
     1) create a 16 x 16 grid by looping through 16 rows and 16 columns and adding a div to the sketchpad
-    2) prompt the user for a number less than 100 and change the grid size according to user input
+    2) make the code in step 1 dynamic so that the user can choose the size of the grid
  END 
 
 */ 
 
 // 1) create a 16 x 16 grid by looping through 16 rows and 16 columns and adding a div to the sketchpad
 
+// let sketchpad = document.querySelector('.sketchpad');
+
+// for (let i = 0; i < 16; i++) {
+//     for (let j = 0; j < 16; j++) {
+//         let box = document.createElement('div');
+//         box.classList.add('box');
+//         sketchpad.appendChild(box);
+//     }
+// }
+// // the width and height of the sketchpad is 500px therefore the width and height of each box is 500/16 = 31.25px
+// let box = document.querySelectorAll('.box');
+// box.forEach((box) => {
+//     box.style.width = '31.25px';
+//     box.style.height = '31.25px';
+// }
+// );
+
+// 2) make the code in step 1 dynamic so that the user can choose the size of the grid
+
 let sketchpad = document.querySelector('.sketchpad');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        sketchpad.appendChild(box);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            sketchpad.appendChild(box);
+        }
     }
+    let box = document.querySelectorAll('.box');
+    box.forEach((box) => {
+        box.style.width = `${500/size}px`;
+        box.style.height = `${500/size}px`;
+    }
+    );
 }
-// the width and height of the sketchpad is 500px therefore the width and height of each box is 500/16 = 31.25px
-let box = document.querySelectorAll('.box');
-box.forEach((box) => {
-    box.style.width = '31.25px';
-    box.style.height = '31.25px';
-}
-);
+let size = prompt('Enter the size of the grid');
+createGrid(size);
